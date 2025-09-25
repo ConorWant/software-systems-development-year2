e131f681dcd0f40b34e3cd31c8a31beefcbdd48d
conor-95 <c.barron4853@student.leedsbeckett.ac.uk>
2025-09-25 10:37:59 +0100

    Created shape class

--- Exercises/lab1/Shape.java
+++ Exercises/lab1/Shape.java
@@ -1,4 +1,48 @@
 package lab1;
 
-abstract class Shape {
+/**
+ * Shape class. Designed to act as an abstract base class to other shapes.
+ *
+ * Shows good use of Javadoc comments, annotations, visibility scope, modifiers and class hierarchies.
+ *
+ * class marked as 'abstract' since it is not designed to be instantiated directly.
+ *
+ * @author mdixon
+ */
+
+abstract class Shape
+{
+    /**
+     * The number of sides within the shape.
+     *
+     * Uses 'private' visablility modifier. . Could use 'protected' so it can be accessed by sub-classess.
+     */
+    private int sides;
+
+    /**
+     * Gets the number of sides within the shape.
+     *
+     * @return the number of sides within the shape.
+     */
+    public int getSides()
+    {
+        return sides;
+    }
+
+    /**
+     * Sets the numer of sides within the shape.
+     *
+     * @param sides the number of sides within the shape.
+     */
+    abstract public int getArea();
+
+    /**
+     * Constructor.
+     *
+     * @param sides the number of sides within the shape.
+     */
+    Shape(int sides)
+    {
+        this.sides = sides;
+    }
 }
